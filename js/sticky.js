12d871573795c02e0c7c1816678e8f11c6d1b794
Vangel Tzo <srekoble@yahoo.gr>
2014-02-26 22:12:42 +0200

    sticky description for photography projects

--- js/sticky.js
+++ js/sticky.js
@@ -1,43 +1,7 @@
----
-layout: layout
-title: "Portfolio"
-group: "photo_navigation"
----
-
-<section class="content photography">
-  <div class="inner-bg">
-    <img src="../../images/test-bg.jpg" alt="">
-    <span>rpk master of photography</span>
-  </div>
-  <div class="center-wrap">
-    <div class="inner-center">
-      <div class="stack-right">
-        <h2>Wood Photography</h2>
-        <small>Location: chalkida</small>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, perspiciatis, repellat, consequatur molestias modi quisquam odit quos animi molestiae recusandae dolores odio omnis nulla porro quis! Rerum, ipsa voluptatibus nobis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, quasi consectetur quos sint sit corporis numquam illum dolor. Odit quas iusto rerum voluptates nihil neque eligendi libero earum facilis nisi.
-        </p>
-      </div>
-      <div class="stack-left">
-  			<p><img src="http://farm8.staticflickr.com/7329/12107997464_7ffab67b9a_c.jpg" alt="1nd image" />
-  			<img src="http://farm8.staticflickr.com/7432/12107567305_d7d09f8d40_c.jpg" alt="2nd image" />
-  			<img src="http://farm6.staticflickr.com/5476/12108302156_3de19272e1_c.jpg" alt="3nd image" />
-  			<img src="http://farm6.staticflickr.com/5538/12175421005_fa8b947131_c.jpg" alt="4nd image" /></p>
-      </div>
-      
-		</div>
-		</div>
-	</section>
-
-  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script>
 // Sticky Plugin v1.0.0 for jQuery - http://stickyjs.com/
 (function(f){var e={topSpacing:0,bottomSpacing:0,className:"is-sticky",wrapperClassName:"sticky-wrapper",center:false,getWidthFrom:""},b=f(window),d=f(document),i=[],a=b.height(),g=function(){var j=b.scrollTop(),q=d.height(),p=q-a,l=(j>p)?p-j:0;for(var m=0;m<i.length;m++){var r=i[m],k=r.stickyWrapper.offset().top,n=k-r.topSpacing-l;if(j<=n){if(r.currentTop!==null){r.stickyElement.css("position","").css("top","");r.stickyElement.parent().removeClass(r.className);r.currentTop=null}}else{var o=q-r.stickyElement.outerHeight()-r.topSpacing-r.bottomSpacing-j-l;if(o<0){o=o+r.topSpacing}else{o=r.topSpacing}if(r.currentTop!=o){r.stickyElement.css("position","fixed").css("top",o);if(typeof r.getWidthFrom!=="undefined"){r.stickyElement.css("width",f(r.getWidthFrom).width())}r.stickyElement.parent().addClass(r.className);r.currentTop=o}}}},h=function(){a=b.height()},c={init:function(j){var k=f.extend(e,j);return this.each(function(){var l=f(this);var m=l.attr("id");var o=f("<div></div>").attr("id",m+"-sticky-wrapper").addClass(k.wrapperClassName);l.wrapAll(o);if(k.center){l.parent().css({width:l.outerWidth(),marginLeft:"auto",marginRight:"auto"})}if(l.css("float")=="right"){l.css({"float":"none"}).parent().css({"float":"right"})}var n=l.parent();n.css("height",l.outerHeight());i.push({topSpacing:k.topSpacing,bottomSpacing:k.bottomSpacing,stickyElement:l,currentTop:null,stickyWrapper:n,className:k.className,getWidthFrom:k.getWidthFrom})})},update:g};if(window.addEventListener){window.addEventListener("scroll",g,false);window.addEventListener("resize",h,false)}else{if(window.attachEvent){window.attachEvent("onscroll",g);window.attachEvent("onresize",h)}}f.fn.sticky=function(j){if(c[j]){return c[j].apply(this,Array.prototype.slice.call(arguments,1))}else{if(typeof j==="object"||!j){return c.init.apply(this,arguments)}else{f.error("Method "+j+" does not exist on jQuery.sticky")}}};f(function(){setTimeout(g,0)})})(jQuery);
 
-  // Fire Sticky
-  $(function(){
-    $(".stack-right").sticky({topSpacing:20});
-  });
-</script>
-
-
+// Fire Sticky
+$(function(){
+      $(".stack-right").sticky({topSpacing:20});
+    });
